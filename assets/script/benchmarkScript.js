f5248b85b64fcda9da15ed44adb46d1249b39907
Gabriele Lucarelli <l-gabri1998@live.it>
2024-10-23 09:57:38 +0200

    modifica

--- assets/script/benchmarkScript.js
+++ assets/script/benchmarkScript.js
@@ -1,43 +1,120 @@
 // TIMER
 
 const timer = function () {
-  const timerElement = document.getElementById('timer')
-  let i = 60
+  let timer = document.getElementById("timer");
+  let i = 60;
 
-  const countdown = setInterval(function () {
+  setInterval(function () {
     if (i > 0) {
-      timerElement.innerHTML = i
-
-      // Change color based on the remaining time
-      if (i > 30) {
-        timerElement.style.color = 'green' // More than 30 seconds
-      } else if (i > 10) {
-        timerElement.style.color = 'yellow' // Between 10 and 30 seconds
-      } else {
-        timerElement.style.color = 'red' // 10 seconds or less
-      }
+      timer.innerHTML = i;
+    } else if (i === 0) {
+      timer.innerHTML = "0";
     } else {
-      timerElement.innerHTML = '0'
-      clearInterval(countdown)
+      clearInterval(countdown);
     }
-    i--
-  }, 1000)
-}
+    i--;
+  }, 1000);
+};
 
-timer()
+timer();
 
-const reverse = function () {
-  const circle = document.getElementById('time_wrapper')
-  let i = 60
+// const reverse = function () {
+//   let circle = document.getElementById('time_wrapper')
+//   let i = 60
+//   setInterval(function () {
+//     if (i > 0) {
+//       circle.classList.add('color')
+//     } else {
+//     }
+//     i--
+//   }, 1000)
+// }
+// reverse()
 
-  const reverseCountdown = setInterval(function () {
-    if (i > 0) {
-      circle.classList.add('color') // Add your desired class here
-    } else {
-      clearInterval(reverseCountdown)
-    }
-    i--
-  }, 1000)
-}
+const questions = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does CPU stand for?",
+    correct_answer: "Central Processing Unit",
+    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What is the most preferred image format used for logos in the Wikimedia database?",
+    correct_answer: ".svg",
+    incorrect_answers: [".png", ".jpeg", ".gif"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In web design, what does CSS stand for?",
+    correct_answer: "Cascading Style Sheet",
+    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What is the code name for the mobile operating system Android 7.0?",
+    correct_answer: "Nougat",
+    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "On Twitter, what is the character limit for a Tweet?",
+    correct_answer: "140",
+    incorrect_answers: ["120", "160", "100"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Linux was first created as an alternative to Windows XP.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "Which programming language shares its name with an island in Indonesia?",
+    correct_answer: "Java",
+    incorrect_answers: ["Python", "C", "Jakarta"],
+  },
+];
 
-reverse()
+window.onload = function () {
+  let title = document.getElementById("title");
+  let button = document.querySelectorAll("bottoni");
+};
